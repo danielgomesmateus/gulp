98d191d491b09a51b059569204090bb2295da771
Daniel Gomes <daniel.analista@outlook.com>
2018-05-01 10:01:19 -0300

    Commit final

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gulp
+
+Estudo sobre o funcionamento do Gulp. Utilizando alguns plugins como:
+gulp-jsmin
+gulp-rename
+gulp-uglifycss
+gulp-imagemin
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,49 @@
 var gulp = require('gulp');
 var customizeBootstrap = require('gulp-customize-bootstrap');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
+var uglifycss = require('gulp-uglifycss');
+var imagemin = require('gulp-imagemin');
 
-gulp.task('default', ['compileBootstrap']);
+gulp.task('default', ['compileBootstrap', 'libJquery', 'js', 'images']);
+
+gulp.task('observer', function() {
+
+	gulp.watch('./dev/styles/**/*', ['compileBootstrap']);
+	gulp.watch('./dev/app/**/*', ['js']);
+});
 
 gulp.task('compileBootstrap', function() {
 
 	return gulp.src('node_modules/bootstrap/less/bootstrap.less')
 		.pipe(customizeBootstrap('./dev/styles/less/*.less'))
 		.pipe(less())
+		.pipe(uglifycss())
+		.pipe(rename({suffix: '.min'}))
 		.pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('libJquery', function() {
+
+	return gulp.src(['./node_modules/jquery/dist/jquery.min.js', './node_modules/angular/angular.min.js'])
+		.pipe(concat('libJquery.js'))
+		.pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('js', function() {
+
+	return gulp.src(['./dev/app/app.js', './dev/app/controllers.js'])
+		.pipe(concat('app.js'))
+		.pipe(jsmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('images', function() {
+
+	return gulp.src(['./dev/images/*'])
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dist/images/'))
 });
